8abb695de5bf003b3a7485c7072dcbf5857d6f14
miko <benjouad@yahoo.fr>
2022-01-27 21:58:17 +0100

    fixed width

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,19 +4,19 @@
 <form  style="font-size:20px;">
 <div class="form-group"  >
     <label >Name :</label>
-    <input width="10%" class="form-control" value="{{Auth::user()->name}}" >
+    <input width="30%" class="form-control" value="{{Auth::user()->name}}" >
   </div><br>
   <div class="form-group">
     <label>Email address :</label>
-    <input type="email" class="form-control" value="{{Auth::user()->email}}" >
+    <input style="width:30%" type="email" class="form-control" value="{{Auth::user()->email}}" >
   </div><br>
   <div class="form-group">
     <label>Password :</label>
-    <input type="password" class="form-control" value="{{Auth::user()->password}}" placeholder="Password">
+    <input style="width:30%" type="password" class="form-control" value="{{Auth::user()->password}}" placeholder="Password">
   </div><br>
   <div class="form-group">
     <label>Leave a message :</label>
-    <textarea class="form-control" rows="4"></textarea>
+    <textarea style="width:40%"class="form-control" rows="4"></textarea>
   </div>
 </form>
 
